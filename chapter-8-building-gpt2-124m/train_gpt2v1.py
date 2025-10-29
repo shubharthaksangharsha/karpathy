05c9c018a8409f2d812653959e351bcadc6f7e14
shubharthaksangharsha <shubharthaksangharsha@gmail.com>
2025-10-29 14:39:33 +1030

    set residual init std:0.02

--- chapter-8-building-gpt2-124m/train_gpt2v1.py
+++ chapter-8-building-gpt2-124m/train_gpt2v1.py
@@ -14,7 +14,7 @@ class CausalSelfAttention(nn.Module):
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd) #bias ON
         # output projection
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)  
-
+        self.c_proj.NANOGPT_SCALE_INIT = 1 
         # regularization
         self.n_head = config.n_head 
         self.n_embd = config.n_embd 
@@ -51,6 +51,7 @@ class MLP(nn.Module):
         self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
         self.gelu = nn.GELU(approximate='tanh')
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1 
 
     def forward(self, x):
         x = self.c_fc(x)
@@ -107,13 +108,14 @@ class GPT(nn.Module):
 
     def _init_weights(self, module):
         if isinstance(module, nn.Linear):
-            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            std = 0.02 
+            if hasattr(module, "NANOGPT_SCALE_INIT"):
+                std *= 2 * (self.config.n_layer) ** -0.5 
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
             if module.bias is not None:
                 torch.nn.init.zeros_(module.bias)   
         elif isinstance(module, nn.Embedding):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
-
-    
 
     def forward(self, idx, targets=None):
         # idx is of shape (B, T)
