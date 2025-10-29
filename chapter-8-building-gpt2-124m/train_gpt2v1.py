fc44e4cdd2700e0ca9f013e339b77f8d2319b14a
shubharthaksangharsha <shubharthaksangharsha@gmail.com>
2025-10-29 13:37:45 +1030

    loaded pretrained model configs with our own custom made settings

--- chapter-8-building-gpt2-124m/train_gpt2v1.py
+++ chapter-8-building-gpt2-124m/train_gpt2v1.py
@@ -11,9 +11,10 @@ class CausalSelfAttention(nn.Module):
         super().__init__()
         assert config.n_embd % config.n_head == 0 
         # key, query, value projections for all heads, but in a batch 
-        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=False)
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd) #bias ON
         # output projection
-        self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=False)  
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)  
+
         # regularization
         self.n_head = config.n_head 
         self.n_embd = config.n_embd 
@@ -62,14 +63,14 @@ class Block(nn.Module):
     
     def __init__(self, config):
         super().__init__() 
-        self.ln1 = nn.LayerNorm(config.n_embd)
+        self.ln_1 = nn.LayerNorm(config.n_embd)
         self.attn = CausalSelfAttention(config)
-        self.ln2 = nn.LayerNorm(config.n_embd)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
         self.mlp = MLP(config)
 
     def forward(self, x):
-        x = x + self.attn(self.ln1(x))
-        x = x + self.mlp(self.ln2(x))
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
         return x 
 
 
@@ -97,3 +98,153 @@ class GPT(nn.Module):
             ln_f = nn.LayerNorm(config.n_embd),
         ))  
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+
+    def forward(self, idx):
+        # idx is of shape (B, T)
+        B, T = idx.size() 
+        assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size"
+        # forward the token and position embeddings 
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device).unsqueeze(0) # shape (T)
+        pos_emb = self.transformer.wpe(pos) # position embeddings of shape (T, n_embd)
+        tok_emb = self.transformer.wte(idx) # token embeddings of shape (B, T, n_embd)
+        x = tok_emb + pos_emb 
+        # forward the blocks of the transformer 
+        for block in self.transformer.h:
+            x = block(x)
+        # forward the final layernorm and the classifer 
+        x = self.transformer.ln_f(x)
+        logits = self.lm_head(x) # (B, T, vocab_size) 
+        return logits 
+        
+
+    @classmethod
+    def from_pretrained(cls, model_type):
+        """Loads pretrained GPT-2 model weights from huggingface"""
+        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+        from transformers import GPT2LMHeadModel
+        print("loading weights from pretrained gpt: %s" % model_type)
+
+        # n_layer, n_head and n_embd are determined from model_type
+        config_args = {
+            'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
+            'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
+            'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
+            'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
+        }[model_type]
+        config_args['vocab_size'] = 50257 # always 50257 for GPT model checkpoints
+        config_args['block_size'] = 1024 # always 1024 for GPT model checkpoints
+        # create a from-scratch initialized minGPT model
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # discard this mask / buffer, not a param
+
+        # init a huggingface/transformers model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        # copy while ensuring all of the parameters are aligned and match in names and shapes
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # ignore these, just a buffer
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # same, just the mask (buffer)
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
+        # this means that we have to transpose these weights when we import them
+        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # special treatment for the Conv1D weights we need to transpose
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                # vanilla copy over the other parameters
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        return model
+
+    # def configure_optimizers(self, weight_decay, learning_rate, device_type):
+    #     # start with all of the candidate parameters (that require grad)
+    #     param_dict = {pn: p for pn, p in self.named_parameters()}
+    #     param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+    #     # create optim groups. Any parameters that is 2D will be weight decayed, otherwise no.
+    #     # i.e. all weight tensors in matmuls + embeddings decay, all biases and layernorms don't.
+    #     decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+    #     nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+    #     optim_groups = [
+    #         {'params': decay_params, 'weight_decay': weight_decay},
+    #         {'params': nodecay_params, 'weight_decay': 0.0}
+    #     ]
+    #     num_decay_params = sum(p.numel() for p in decay_params)
+    #     num_nodecay_params = sum(p.numel() for p in nodecay_params)
+    #     if master_process:
+    #         print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+    #         print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+    #     # Create AdamW optimizer and use the fused version if it is available
+    #     fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+    #     use_fused = fused_available and device_type == "cuda"
+    #     if master_process:
+    #         print(f"using fused AdamW: {use_fused}")
+    #     optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8, fused=use_fused)
+    #     return optimizer
+
+# -----------------------------------------------------------------------------
+# attempt to autodetect the device 
+device = "cpu"
+if torch.cuda.is_available():
+    device = "cuda"
+elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+    device = "mps"
+
+print(f"using device: {device}")
+
+max_length = 30
+num_return_sequences = 5 
+
+
+# model = GPT.from_pretrained("gpt2")
+model = GPT(GPTConfig())
+model.eval()
+model.to(device)
+
+#prefix tokens 
+import tiktoken 
+enc = tiktoken.get_encoding("gpt2")
+tokens = enc.encode("Hello, I'm a language model,")
+tokens = torch.tensor(tokens, dtype=torch.long) #(8, )
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1) #(5, 8)
+x = tokens.to(device)
+
+
+# generate! right now x is (B, T) where B = 5, T = 8 
+# set the seed is 42 (my fav what is life :P)
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+while x.size(1) < max_length:
+    # forward the model to get the logits 
+    with torch.no_grad():
+        logits = model(x) # (B, T, vocab_size)
+        # take the logits at the last position 
+        logits = logits[:, -1, :] # (B, vocab_size) 
+        # get the probabilities 
+        probs = F.softmax(logits, dim=-1)
+        # do top-k sampling of 50 (huggingface pipeline default) 
+        # topk_probs here becomes (5, 50), topk_indices is  (5, 50) 
+        topk_probs, topk_indices = torch.topk(probs, k=50, dim=-1)
+        # select a token from the top-k probabilities 
+        ix = torch.multinomial(topk_probs, num_samples=1) # (B, 1) 
+        # gather the corresponding indices 
+        xcol = torch.gather(topk_indices, -1, ix) # (B, 1)
+        # append to the sequnce 
+        x = torch.cat((x, xcol), dim=1)
+
+# print the generated text 
+for i in range(num_return_sequences):
+    tokens = x[i, :max_length].tolist() 
+    decoded = enc.decode(tokens)
+    print(">", decoded)
+
